9494ea86644515368c0186a132b8d627755b89a0
Prakhar <agrawalprakhar031@gmail.com>
2024-07-07 19:26:59 +0530

    setup basic structure

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const width = 10
+    const scoreDisplay = document.querySelector('#score')
+    const Startbtn = document.querySelector('#start-button')
+}
+
+)
